7f16571ad8c6079c17e6148ec798b69b477a7917
moseslucas <mo@moseslucas.com>
2018-02-28 18:40:52 +0800

    Set activeadmin as root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'product#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'admin/dashboard#index'
 end
